7a4df90df21ee07b0082f8ee3418316e08ff1ffc
Nguyen Quang Huy <kaisa13102004@gmail.com>
2024-06-07 23:42:39 +0700

    Chat settings

---
+++ app/(dashboard)/u/[username]/[chat]/_components/toggle-card.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { toast } from 'sonner'
+import { useTransition } from 'react'
+
+import { Switch } from '@/components/ui/switch'
+import { updateStream } from '@/actions/steam'
+import { Skeleton } from '@/components/ui/skeleton'
+
+type FieldTypes = 'isChatEnabled' | 'isChatDelayed' | 'isChatFollowersOnly'
+
+interface ToggleCardProps {
+	label: string
+	value: boolean
+	field: FieldTypes
+}
+
+export const ToggleCard = ({ label, value, field }: ToggleCardProps) => {
+	const [isPedding, startTransition] = useTransition()
+
+	const handleChange = () => {
+		startTransition(() => {
+			updateStream({ [field]: !value })
+				.then(() => toast.success('Chat settings updated!'))
+				.catch(() => toast.error('Something went wrong'))
+		})
+	}
+
+	return (
+		<div className="rounded-xl bg-muted p-6">
+			<div className="flex items-center justify-between">
+				<p className="font-semibold shrink-0">{label}</p>
+				<div className="space-y-2">
+					<Switch onCheckedChange={handleChange} checked={value} disabled={isPedding}>
+						{value ? 'On' : 'Off'}
+					</Switch>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export const ToggleCardSkeleton = () => {
+	return <Skeleton className="rounded-xl p-10 w-full"></Skeleton>
+}
